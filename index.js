d5cef18746a613e4724ee94ff02828800f03bc2f
mort-yn <kimumartin.9@yahoo.com>
2025-09-06 15:32:17 +0300

    Updated css and html pages and added media

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ function hideSidebar() {
     let sidebar = document.getElementsByClassName("sidebar");
     sidebar[0].style.display = "none";
 } 
+
+document.getElementById("year").innerText = new Date().getFullYear();
+
+document.querySelectorAll(".clickable").forEach(img => {
+    img.addEventListener("click", () => {
+        window.location.href = img.getAttribute("data-url");
+    });
+});
